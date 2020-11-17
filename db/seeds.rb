0f390bf689ac653c4074d081d53218ee8c62150a
margaux stanislas <margaux.stanislas@hotmail.fr>
2020-11-17 07:05:46 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "seeding 10 articles"
+10.times do 
+  Article.create(title: Faker::Hipster.sentence, content: Faker::Quote.famous_last_words  )
+end
+puts "seeding done"
